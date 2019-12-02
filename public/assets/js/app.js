3822a31b8a129b3718cbc5c6f655508ccf761d1e
Tamás Zsanett <zsanett.tamas87@gmail.com>
2019-12-02 23:12:59 +0100

    Header fixes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -43,6 +43,26 @@ function toggleMenuFunction () {
 //     });
 // }
 
+
+//tabs
+
+function tabSize() {
+
+    // get tallest tab__content element
+    let height = -1;
+
+    $('.tab__content').each(function() {
+        height = height > $(this).outerHeight() ? height : $(this).outerHeight();
+        $(this).css('position', 'absolute');
+    });
+
+    // set height of tabs + top offset
+    $('.tabs').css('min-height', height + 40 + 'px');
+
+}
+
+
 $(document).ready(function(){
     toggleMenuFunction();
+    tabSize();
 });
